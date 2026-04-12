9e6a18d22cb23c5675308ba1da2772b8f3306e35
WLAngel <akloratok@gmail.com>
2026-04-13 00:17:04 +0800

    feat(keymaps): remove esc -> noh

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,7 @@
 -- Remove LazyVim's <leader>c* keymaps defined in lazyvim/config/keymaps.lua
 vim.keymap.del({ "n", "x" }, "<leader>cf")
 vim.keymap.del("n", "<leader>cd")
+vim.keymap.del({ "i", "n", "s" }, "<esc>")
 
 -- Visual mode: copy selection to the active clipboard provider, then move cursor to end of selection
 vim.keymap.set("v", "<leader>c", '"+y`]', { desc = "Copy to clipboard", noremap = true, silent = true })
